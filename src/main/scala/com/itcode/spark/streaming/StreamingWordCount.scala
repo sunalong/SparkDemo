88cdd17f6ff0fe0b356a0d1ad605f7855cb90adf
sunalong <sunalong@ztgame.com>
2018-04-26 12:22:42 +0800

    使用spark streaming读取mini1的9999端口发送的数据并统计

--- src/main/scala/com/itcode/spark/streaming/StreamingWordCount.scala
+++ src/main/scala/com/itcode/spark/streaming/StreamingWordCount.scala
@@ -13,16 +13,20 @@ import org.apache.spark.{SparkConf, SparkContext}
 object StreamingWordCount {
   def main(args: Array[String]): Unit = {
     LoggerLevelsUtils.setStreamingLogLevels()
+    //创建SparkConf并设置为本地模式运行，注意local[2]代表开两个线程
     val conf = new SparkConf().setAppName(this.getClass.getSimpleName).setMaster("local[2]")
     val sc = new SparkContext(conf)
+    //设置DStream批次时间间隔为2秒
     val ssc = new StreamingContext(sc, Seconds(5))
-    //接收数据
-    val ds = ssc.socketTextStream("mini1", 9999)
+    //通过网络读取数据
+    val lines = ssc.socketTextStream("mini1", 9999)
     //DStream 是一个特殊的RDD
-    val result = ds.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+    val result = lines.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
     //打印结果
     result.print()
+    //开始计算
     ssc.start()
+    //等待停止
     ssc.awaitTermination()
   }
 
